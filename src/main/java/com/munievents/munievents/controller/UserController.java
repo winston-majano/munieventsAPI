35e480a1d6b8cb5cdd5df380b47ff2a37129f806
RamonLGDaw <ramonlg.daw@gmail.com>
2024-04-16 21:13:27 +0200

    se agrega userService

--- src/main/java/com/munievents/munievents/controller/UserController.java
+++ src/main/java/com/munievents/munievents/controller/UserController.java
@@ -6,56 +6,55 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.munievents.munievents.entity.User;
-import com.munievents.munievents.repository.UserRepository;
+import com.munievents.munievents.service.UserService;
 
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
 @RestController
+@RequestMapping(path = "api/v1")
 public class UserController {
 
+    private final UserService userService;
+
     @Autowired
-    UserRepository ur;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/users")
     public List<User> all() {
-        return this.ur.findAll();
+        return userService.getAll();
     }
 
     @GetMapping("/users/{id}")
-    public User oneById(@PathVariable("id") int id) {
-        Optional<User> found = this.ur.findById(id);
-        if(found.isEmpty()){
-            return null;
-        }
-        return found.get();
+    public User oneById(@PathVariable int id) {
+        return userService.oneById(id).orElse(null);
     }
 
     @PostMapping("/users")
-    public String add(@RequestBody User newUser) {
-        User dbUser = this.ur.saveAndFlush(newUser);
-        return "Usuario creado con id" + dbUser.getId();
+    public User add(@RequestBody User newUser) {
+        return userService.saveAndFlush(newUser);
     }
 
     @DeleteMapping("/users/{id}")
-    public String deleteUserById(@PathVariable("id") int id) {
-        this.ur.deleteById(id);
+    public String deleteUserById(@PathVariable int id) {
+        userService.deleteById(id);
         return "Usuario Eliminado";
     }
 
     @PutMapping("/users/{id}")
     public String updateUser(@PathVariable int id, @RequestBody User usuario) {
-        User u = this.oneById(id);
+        User u = userService.oneById(id).orElse(null);
         if (u == null) {
             return "No encontrado";
         }
         usuario.setId(id);
-        this.ur.save(usuario);
+        userService.save(usuario);
         return "OK";
     }
-
 }
---
+++ src/main/java/com/munievents/munievents/service/UserService.java
@@ -0,0 +1,45 @@
+package com.munievents.munievents.service;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import com.munievents.munievents.entity.User;
+import com.munievents.munievents.repository.UserRepository;
+
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    //Devuelve todos los usuarios
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    //Devuelve un usuario seleccionado por ID
+    public Optional<User> oneById(int id) {
+        return userRepository.findById(id);
+    }
+
+    //Crear un nuevo usuario
+    public User saveAndFlush(User newUser) {
+        return userRepository.saveAndFlush(newUser);
+    }
+
+    //Elimina un usuario por su ID
+    public void deleteById(int id) {
+        userRepository.deleteById(id);
+    }
+
+    //Actualiza los datos de un usuario
+    public User save(User usuario) {
+        return userRepository.save(usuario);
+    }
+}
