0c757bb53a2997c38154d51e5cbf303e6a1c6afa
winston-majano <winstonmajano@gmail.com>
2024-05-06 14:15:09 +0200

    crossorigin para planes

--- src/main/java/com/munievents/munievents/MunieventsApplication.java
+++ src/main/java/com/munievents/munievents/MunieventsApplication.java
@@ -2,9 +2,16 @@ package com.munievents.munievents;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MunieventsApplication {
+public class MunieventsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MunieventsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MunieventsApplication.class, args);
