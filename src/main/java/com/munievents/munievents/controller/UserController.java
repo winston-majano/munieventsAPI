fe1f3103e2857ed6e588901d2cb9ad4642b75b6f
RamonLGDaw <ramonlg.daw@gmail.com>
2024-05-27 11:38:04 +0200

    cambios para usar files y guardar en directorio uploads

--- src/main/java/com/munievents/munievents/controller/UserController.java
+++ src/main/java/com/munievents/munievents/controller/UserController.java
@@ -1,23 +1,25 @@
+
 package com.munievents.munievents.controller;
 
-import java.util.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import com.munievents.munievents.entity.User;
 import com.munievents.munievents.entity.UserLogin;
 import com.munievents.munievents.entity.UserUpdate;
-import com.munievents.munievents.entity.User;
 import com.munievents.munievents.service.UserService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping(path = "/api/v1")
@@ -26,22 +28,18 @@ import com.munievents.munievents.service.UserService;
 public class UserController {
 
     private final UserService userService;
+    private static final String UPLOAD_DIR = "uploads/";
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    // @GetMapping("/users")
-    // public List<User> getAll() {
-    // return userService.getAll();
-    // }
-
-    // Para retornar solo los usuarios activos.
     @GetMapping("/users")
     public List<User> getAll() {
         return userService.getAllActive();
     }
 
+<<<<<<< Updated upstream
     //Comentario de ejemplo
 
     // @GetMapping("/users/{id}")
@@ -50,6 +48,8 @@ public class UserController {
     // }
 
     // Retorna los usuarios por ID que tengan el status activo
+=======
+>>>>>>> Stashed changes
     @GetMapping("/users/{id}")
     public ResponseEntity<User> oneById(@PathVariable int id) {
         Optional<User> userOptional = userService.oneByIdActive(id);
@@ -59,18 +59,6 @@ public class UserController {
             return ResponseEntity.notFound().build();
         }
     }
-
-    @PostMapping("/users")
-    public ResponseEntity<User> add(@RequestBody User newUser) {
-
-        return userService.saveAndFlush(newUser);
-    }
-
-    // @DeleteMapping("/users/{id}")
-    // public String deleteUserById(@PathVariable int id) {
-    // userService.deleteById(id);
-    // return "Usuario Eliminado";
-    // }
 
     @DeleteMapping("/users/{id}")
     public String deactivateUserById(@PathVariable int id) {
@@ -85,77 +73,6 @@ public class UserController {
         }
     }
 
-    // PutMapping que funcionaba antes de bloquear el password en el get
-
-    // @PutMapping("/users/{id}")
-    // public String updateUser(@PathVariable int id, @RequestBody User usuario) {
-    // Optional<User> userUpdate = userService.oneById(id);
-    // if (!userUpdate.isPresent()) {
-    // return "No se ha actualizado correctamente";
-    // }
-    // usuario.setId(id);
-    // userService.save(usuario);
-    // return "OK, usuario actualizado";
-    // }
-
-    // @PutMapping("/users/{id}")
-    // public String updateUser(@PathVariable int id, @RequestBody UserUpdate
-    // userUpdate) {
-    // Optional<User> userUpgrade = userService.oneById(id);
-    // if (!userUpgrade.isPresent()) {
-    // return "No se ha actualizado correctamente";
-    // }
-
-    // User usuario = userUpgrade.get();
-    // usuario.setImage_user(userUpdate.getImage_user());
-    // usuario.setEmail(userUpdate.getEmail());
-    // usuario.setPhone(userUpdate.getPhone());
-    // usuario.setFull_name(userUpdate.getFull_name());
-    // usuario.setAlias(userUpdate.getAlias());
-    // usuario.setQty_event_sub(userUpdate.getQty_event_sub());
-
-    // userService.save(usuario);
-    // return "OK, usuario actualizado";
-    // }
-
-    @PutMapping("/users/{id}")
-    public ResponseEntity<String> updateUser(@PathVariable int id, @RequestBody UserUpdate userUpdate) {
-        Optional<User> userOptional = userService.oneByIdActive(id);
-        if (!userOptional.isPresent()) {
-            return ResponseEntity.notFound().build();
-        }
-
-        User usuario = userOptional.get();
-        usuario.setImage_user(userUpdate.getImage_user());
-        usuario.setEmail(userUpdate.getEmail());
-        usuario.setPhone(userUpdate.getPhone());
-        usuario.setFull_name(userUpdate.getFull_name());
-        usuario.setAlias(userUpdate.getAlias());
-        usuario.setQty_event_sub(userUpdate.getQty_event_sub());
-
-        userService.save(usuario);
-        return ResponseEntity.ok("OK, usuario actualizado");
-    }
-
-    // @PostMapping("/usersLogin")
-    // public ResponseEntity<User> login(@RequestBody UserLogin userLogin) {
-    //     Optional<User> usuarioOptional = userService.findOneByEmail(userLogin.getEmail());
-    //     if (usuarioOptional.isPresent()) {
-    //         User usuario = usuarioOptional.get();
-    //         if (usuario.getPassword().equals(userLogin.getPassword())) {
-    //             // Devuelve el objeto User en el cuerpo de la respuesta si la contraseña es
-    //             // correcta
-    //             return ResponseEntity.ok(usuario);
-    //         } else {
-    //             // Devuelve un estado de no autorizado si la contraseña es incorrecta
-    //             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-    //         }
-    //     } else {
-    //         // Devuelve un estado de no encontrado si el usuario no existe
-    //         return ResponseEntity.notFound().build();
-    //     }
-    // }
-
     @PostMapping("/usersLogin")
     public ResponseEntity<User> login(@RequestBody UserLogin userLogin) {
         Optional<User> usuarioOptional = userService.findOneByEmailAndStatus(userLogin.getEmail(), "A");
@@ -169,5 +86,118 @@ public class UserController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    // @PostMapping("/users")
+    // public ResponseEntity<User> add(
+    // @RequestParam("full_name") String fullName,
+    // @RequestParam("email") String email,
+    // @RequestParam("phone") String phone,
+    // @RequestParam("alias") String alias,
+    // @RequestParam("password") String password,
+    // @RequestParam("image_user") MultipartFile imageFile) {
+    // String imageUrl = saveImage(imageFile);
+    // if (imageUrl == null) {
+    // return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    // }
+
+    // User newUser = new User();
+    // newUser.setFull_name(fullName);
+    // newUser.setEmail(email);
+    // newUser.setPhone(phone);
+    // newUser.setAlias(alias);
+    // newUser.setPassword(password);
+    // newUser.setImage_user(imageUrl);
+
+    // return userService.saveAndFlush(newUser);
+    // }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> add(
+            @RequestParam("full_name") String fullName,
+            @RequestParam("email") String email,
+            @RequestParam("phone") String phone,
+            @RequestParam("alias") String alias,
+            @RequestParam("password") String password,
+            @RequestParam(value = "image_user", required = false) MultipartFile imageFile) {
+        String imageUrl = (imageFile != null && !imageFile.isEmpty()) ? saveImage(imageFile) : null;
+
+        User newUser = new User();
+        newUser.setFull_name(fullName);
+        newUser.setEmail(email);
+        newUser.setPhone(phone);
+        newUser.setAlias(alias);
+        newUser.setPassword(password);
+        newUser.setImage_user(imageUrl);
+
+        return userService.saveAndFlush(newUser);
+    }
+
+    // @PutMapping("/users/{id}")
+    // public ResponseEntity<User> updateUser(@PathVariable int id,
+    //         @RequestParam("full_name") String fullName,
+    //         @RequestParam("email") String email,
+    //         @RequestParam("phone") String phone,
+    //         @RequestParam("alias") String alias,
+    //         @RequestParam("image_user") MultipartFile imageFile) {
+    //     Optional<User> userOptional = userService.oneByIdActive(id);
+    //     if (!userOptional.isPresent()) {
+    //         return ResponseEntity.notFound().build();
+    //     }
+
+    //     User usuario = userOptional.get();
+    //     String imageUrl = saveImage(imageFile);
+    //     if (imageUrl != null) {
+    //         usuario.setImage_user(imageUrl);
+    //     }
+    //     usuario.setEmail(email);
+    //     usuario.setPhone(phone);
+    //     usuario.setFull_name(fullName);
+    //     usuario.setAlias(alias);
+
+    //     User updatedUser = userService.save(usuario);
+    //     return ResponseEntity.ok(updatedUser);
+    // }
+
+    private String saveImage(MultipartFile file) {
+        if (file.isEmpty()) {
+            return null;
+        }
+
+        try {
+            String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+            Path path = Paths.get(UPLOAD_DIR + fileName);
+            Files.createDirectories(path.getParent());
+            Files.write(path, file.getBytes());
+            return fileName; // Solo el nombre del archivo
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @PutMapping("/users/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable int id,
+            @RequestParam("full_name") String fullName,
+            @RequestParam("email") String email,
+            @RequestParam("phone") String phone,
+            @RequestParam("alias") String alias,
+            @RequestParam(value = "image_user", required = false) MultipartFile imageFile) {
+        Optional<User> userOptional = userService.oneByIdActive(id);
+        if (!userOptional.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        User usuario = userOptional.get();
+        String imageUrl = (imageFile != null && !imageFile.isEmpty()) ? saveImage(imageFile) : usuario.getImage_user();
+
+        usuario.setEmail(email);
+        usuario.setPhone(phone);
+        usuario.setFull_name(fullName);
+        usuario.setAlias(alias);
+        usuario.setImage_user(imageUrl);
+
+        User updatedUser = userService.save(usuario);
+        return ResponseEntity.ok(updatedUser);
     }
 }
--- src/main/java/com/munievents/munievents/entity/User.java
+++ src/main/java/com/munievents/munievents/entity/User.java
@@ -4,36 +4,24 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Getter
-@Setter
+
+@Getter 
+@Setter 
 @Entity
 @Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String image_user;
+    private String image_user; // Almacena la ruta del archivo
     private String email;
     private String phone;
     private String full_name;
     private String alias;
     private int qty_event_sub;
-    //@JsonIgnoreProperties(value="password")
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     private String status;
 
-
-  /*  @ManyToMany(cascade =  CascadeType.ALL )
-    @JoinTable(
-        name = "user_rol",
-        joinColumns =  @JoinColumn(name = "users_id"),
-        inverseJoinColumns = @JoinColumn(name = "user_rol_id") )
-    //TODO: el JsonIgnoreProperties sirve para decirle al objeto json que propiedad no quires que se muestre
-    @JsonIgnoreProperties(value="users")
-    private Set<Rol> rol;
-*/
-
-
-
+    
 }
